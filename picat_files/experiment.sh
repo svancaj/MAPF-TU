bace22478c9610f6520f52b4f87cbc0860243969
svancaj <jirka.svanc@gmail.com>
2024-11-18 08:40:10 +0100

    experiments with timeout 300

--- picat_files/experiment.sh
+++ picat_files/experiment.sh
@@ -8,7 +8,7 @@ do
         name=$(echo $file | cut -d/ -f2)
         echo solving $name
 
-        timeout 100 ./picat $solver $file > tmp
+        timeout 300 ./picat $solver $file > tmp
 
         cost=$(grep "Cost" tmp | cut -d" " -f2)
         el_time=$(grep CPU tmp | cut -d" " -f3)
